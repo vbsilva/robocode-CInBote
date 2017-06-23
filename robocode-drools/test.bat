7ce5d29badc7c3a7125871ad98c93afc4426aa5e
casc2 <casc2@cin.ufpe.br>
2017-06-23 01:56:10 -0300

    Remove robocode-drools duplicated repository

--- robocode-drools/test.bat
+++
@@ -1,8 +0,0 @@
-@echo off
- setLocal EnableDelayedExpansion
- set CLASSPATH="
- for /R ./target/dependencies %%a in (*.jar) do (
-   set CLASSPATH=!CLASSPATH!;%%a
- )
- set CLASSPATH=!CLASSPATH!"
- echo !CLASSPATH!
